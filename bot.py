e12548cb7b8962c2a9d520fd2d5f641823b845c4
yamadalucifer <luciferissopity@gmail.com>
2024-10-06 12:53:18 +0900

    silent機能

--- bot.py
+++ bot.py
@@ -25,7 +25,6 @@ intents.message_content = True
 selected_text_channel = {}
 selected_voice_channel = {}
 
-g_str = ""
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -60,20 +59,18 @@ async def fetch_messages(channel_id):
         #print(f"{message.author}: {message.content}")
         str+=f"{message.author}: {message.content}\n"
 
-    global g_str
-    g_str = str[:]
+    return str
     #print(g_str)
          
 async def summarize(channel_id, prompt, title):
     try:
-        global g_str
         #print("g_str")
         #print(g_str)
         channel = client.get_channel(channel_id)
         if channel is None:
             print("channel is none")
             return
-        str = prompt + g_str
+        str = prompt
         #print(str)
         model = GenerativeModel("gemini-pro")
         gt = model.generate_content(str)
@@ -84,32 +81,56 @@ async def summarize(channel_id, prompt, title):
             description=gt.text,
             color=0x00ff00
         )
-        await channel.send(embed=embed)
+        return embed
+        #await channel.send(embed=embed)
     except Exception as e:
         print(e)
 
-#await interaction.response.send_message(f"Current Channel id: {channel_id}")
 @client.tree.command(name="要約", description="過去一日分のメッセージを要約します")
 async def get_messages(interaction: discord.Interaction):
     try:
         channel_id = interaction.channel_id
         await interaction.response.defer()
-        await fetch_messages(channel_id)
-        await summarize(channel_id,"次の文章を6000字以内で要約してください：","要約結果")
-        await interaction.followup.send("処理が終了しました")
+        str = await fetch_messages(channel_id)
+        embed = await summarize(channel_id,"次の文章を6000字以内で要約してください：\n"+str,"要約結果")
+        await interaction.followup.send(embed=embed)
     except Exception as e:
         print(e)
+    await interaction.followup.send("処理が終了しました")
 
 @client.tree.command(name="mvp", description="24時間のMVP")
 async def mvp(interaction: discord.Interaction):
     try:
         channel_id = interaction.channel_id
         await interaction.response.defer()
-        await fetch_messages(channel_id)
-        await summarize(channel_id,"次の文章からMVP(最も格好良い発言)を選んでください：","MVP")
-        await interaction.followup.send("処理が終了しました")
+        str = await fetch_messages(channel_id)
+        embed = await summarize(channel_id,"次の文章からMVP(最も格好良い発言)を選んでください：\n"+str,"MVP")
+        await interaction.followup.send(embed=embed)
     except Exception as e:
         print(e)
+    await interaction.followup.send("処理が終了しました")
+
+@client.tree.command(name="silent要約", description="要約結果をあなただけにお届け")
+async def silent_get_messages(interaction: discord.Interaction):
+    try:
+        channel_id = interaction.channel_id
+        await interaction.response.defer(ephemeral=True)
+        str = await fetch_messages(channel_id)
+        embed = await summarize(channel_id,"次の文章を6000字以内で要約してください：\n"+str,"要約結果")
+        await interaction.followup.send(embed=embed)
+    except Exception as e:
+        print(e) 
+
+@client.tree.command(name="silent_mvp", description="mvpをあなただけにお届け")
+async def silent_mvp(interaction: discord.Interaction):
+    try:
+        channel_id = interaction.channel_id
+        await interaction.response.defer(ephemeral=True)
+        str = await fetch_messages(channel_id)
+        embed = await summarize(channel_id,"次の文章からMVP(最も格好良い発言)を選んでください：\n"+str,"MVP")
+        await interaction.followup.send(embed=embed)
+    except Exception as e:
+        print(e) 
 
 @client.tree.command(name="vc参加", description="BOTをVCに参加させます")
 async def join(interaction: discord.Interaction):
